c868fa17ac78b3f27c420dd6f6ee1669fabc5781
RajithaThilanka <rajithat1998@gmail.com>
2023-02-17 11:00:35 +0530

    'feedinit'

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home/Home";
 import { useSelector } from "react-redux";
 
 import Auth from "./pages/Auth/Auth";
+import Userfeed from "./pages/UserFeed/Userfeed";
+import Profile from './pages/UserProfile/UserProfile';
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
   return (
@@ -11,17 +13,29 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={user ? <Navigate to="/home" /> : <Navigate to="/auth" />}
+          element={<Home/>}
         ></Route>
         <Route
           path="/home"
-          element={user ? <Home /> : <Navigate to="/auth" />}
+          element={<Home />}
         />
         <Route
           path="/auth"
           element={user ? <Navigate to="/home" /> : <Auth />}
         />
+        
+        <Route
+          path="/feed"
+          element={ < Userfeed/>}
+        />
+     
+        <Route
+          path="/profile"
+          element={ <Profile />}
+        />
       </Routes>
+      
+
     </div>
   );
 }
