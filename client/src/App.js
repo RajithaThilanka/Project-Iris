fbc82a07aa52b073fe0db0fe6514f36cf0996799
Zeta201 <dini2040gh@gmail.com>
2023-02-18 12:32:09 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import Home from "./pages/Home/Home";
 import { useSelector } from "react-redux";
 
 import Auth from "./pages/Auth/Auth";
+import SignUp from "./components/SignUp/SignUp";
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
   return (
     <div>
-      <Routes>
+      {/* <Routes>
         <Route
           path="/"
           element={user ? <Navigate to="/home" /> : <Navigate to="/auth" />}
@@ -21,7 +22,8 @@ function App() {
           path="/auth"
           element={user ? <Navigate to="/home" /> : <Auth />}
         />
-      </Routes>
+      </Routes> */}
+      <SignUp />
     </div>
   );
 }
