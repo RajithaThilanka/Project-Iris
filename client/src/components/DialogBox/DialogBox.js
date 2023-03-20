f774e7a7b77fbe122337ec07554c236baeb6c8fc
Zeta201 <dini2040gh@gmail.com>
2023-03-20 21:28:46 +0530

    added confirmation dialogs

---
+++ client/src/components/DialogBox/DialogBox.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { IconButton } from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function DialogBox({
+  children,
+  title,
+  content,
+  YesBtn,
+  NoBtn,
+  handleYes,
+  handleNo,
+}) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleAgree = () => {
+    handleYes();
+    setOpen(false);
+  };
+  const handleDisagree = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <span onClick={handleClickOpen}>{children}</span>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {content}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDisagree}>{NoBtn}</Button>
+          <Button onClick={handleAgree}>{YesBtn}</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
