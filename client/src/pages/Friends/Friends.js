5aa7056b6db21379654a246680eca3134e7f7e54
Zeta201 <dini2040gh@gmail.com>
2023-03-20 20:23:54 +0530

    loading and erros for other pages

--- client/src/pages/Friends/Friends.js
+++ client/src/pages/Friends/Friends.js
@@ -9,15 +9,19 @@ import VerticalNavbar from "../../components/VerticalNavbar/VerticalNavbar";
 import Navbar from "../../components/Appbar/Navbar";
 import { useSelector } from "react-redux";
 import BottomNavbar from "../../components/BottomNavbar/BottomNavbar";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 function Friends() {
   const { friends, setFriends } = useContext(MatchesContext);
   const { dates, setDates } = useContext(MatchesContext);
   const { activeTab, setActiveTab } = useContext(MatchesContext);
+
   const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(false);
   setActiveTab(2);
   useEffect(() => {
     const fetchFriends = async () => {
       setLoading(true);
+      setErr(false);
       try {
         const {
           data: {
@@ -26,10 +30,12 @@ function Friends() {
         } = await getAllFriends();
 
         setFriends(data);
+        setErr(false);
         setLoading(false);
       } catch (error) {
         console.log(error);
         setLoading(false);
+        setErr(true);
       }
     };
     fetchFriends();
@@ -37,13 +43,23 @@ function Friends() {
 
   useEffect(() => {
     const fetchDates = async () => {
-      const {
-        data: {
-          data: { data },
-        },
-      } = await getAllDates();
+      setLoading(true);
+      setErr(false);
+      try {
+        const {
+          data: {
+            data: { data },
+          },
+        } = await getAllDates();
 
-      setDates(data);
+        setDates(data);
+        setErr(false);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setErr(true);
+      }
     };
     fetchDates();
   }, []);
@@ -65,12 +81,16 @@ function Friends() {
         }}
       >
         <VerticalNavbar />
-        {!loading && friends.length > 0 && (
+        {!loading && !err && friends.length > 0 ? (
           <div className="friends">
             <ProfileCards cardType="friend" />
           </div>
-        )}
-        {loading && (
+        ) : !loading && !err && friends.length === 0 ? (
+          <h3 className="connections-err-msg">
+            No any friends yet
+            <SentimentVeryDissatisfiedIcon fontSize="large" />
+          </h3>
+        ) : loading && !err ? (
           <div
             className="dashboard-loading-container"
             style={{ height: "100vh" }}
@@ -80,7 +100,15 @@ function Friends() {
               <img src={serverPublic + "irislogo.png"} alt="loading-user" />
             </div>
           </div>
+        ) : !loading && err ? (
+          <h3 className="connections-err-msg">
+            Something went wrong
+            <SentimentVeryDissatisfiedIcon fontSize="large" />
+          </h3>
+        ) : (
+          ""
         )}
+
         <BottomNavbar />
       </div>
     </>
