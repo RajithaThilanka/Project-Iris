10f5e4beb4eabfedaccff9d43c74e48416488c32
rusithliyanage <liyanagerusith2018@gmail.com>
2023-03-22 14:29:22 +0530

    error handled

--- client/src/pages/Questions/QuestionPage.js
+++ client/src/pages/Questions/QuestionPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { Container } from '@mui/material';
-import Navbar from "../../components/Appbar/Navbar";
+import Question from "../../components/Question/Question";
 
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -10,6 +10,8 @@ function QuestionPage() {
     return (
         <div className="Question">
             <h2>Questions</h2>
+            
+            
             <Box
                 sx={{
                     display: 'flex',
@@ -21,11 +23,13 @@ function QuestionPage() {
                     },
                     
                   }}
-            >
+                >
                 <Paper elevation={3}
                     
                 />
-
+                {/* <Question>
+                  Hello
+                </Question> */}
             </Box>
         </div>
     )
