a27bd5180f81ee6f4084d403b3ec123cef559ae3
rusithliyanage <liyanagerusith2018@gmail.com>
2023-03-29 11:22:43 +0530

    question component updated

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -5,7 +5,7 @@ import {
   FormLabel,
   Stack,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
@@ -18,11 +18,11 @@ import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import QuestionHeader from "../../components/Question/QuestionHeader";
 import QuestionAnswers from "./QuestionAnswers";
 import "./Question.css";
-
 import { getQuestionArray } from "../../api/QuestionRequests";
-import { signupAccountInfo } from "../../api/AuthRequests";
+import { signupAccountInfo, addAnswer, signupLookingforInfo } from "../../api/AuthRequests";
 
 function Question() {
+    const { id } = useParams();
     const [allQuestions, setAllQuestions] = useState(null)
     
     useEffect(() => {
@@ -34,31 +34,41 @@ function Question() {
     fetchQuestions();
 
     }, []);
+  
     const [formData, setData] = useState({
         answer: "",
     });
     const [err, setErr] = useState(null);
     const [loading, setLoading] = useState(false);
-    
+
+    const [newQuestion, setQuestion] = useState();
+    // const setNextQuestion = () => {
+    //     setQuestion(allQuestions[2].question);
+    // }
+
     const navigate = useNavigate();
     const handleData = (e) => {
         setData({ ...formData, [e.target.name]: e.target.value });
+        addAnswer(formData);
+        // setQuestion(allQuestions[2].question);
+        console.log("handle Data");
+        //console.log(newQuestion);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
         setErr(null);
-        if (!formData.answer === null) {
+        if (!formData.answer == null) {
         try {
             const {
             data: {
                 data: { data },
             },
-            } = await signupAccountInfo(formData);
+            } = await signupLookingforInfo(formData);
             setErr(null);
             setLoading(false);
-            navigate(`/auth/signup/user-info/${data._id}`);
+            navigate(`/auth/signup/lookingfor-info/${id}`, { replace: true });
         } catch (error) {
             console.log(error);
             setErr(error);
@@ -70,13 +80,30 @@ function Question() {
     };
     // const QuestionAnswersArray = allQuestions[1].question;
     
-    // const QuestionAnswersArray = Object.keys(allQuestions).map((key) => {
-    // return allQuestions[key].answerTags ;
+    // const allAnswersArray = Object.keys(allQuestions.answerTags).map((key) => {
+    //     return allQuestions[key].answerTags[key] ;
     // });
-    const QuestionAnswersArray = Object.keys(allQuestions).map((key) => {
-        return allQuestions[key].answerTags;
-        });
-    console.log('hello', QuestionAnswersArray);
+    // const QuestionAnswersArray = Object.keys(allQuestions).map((key) => {
+    //     return allQuestions[key].answerTags;
+    //     });
+    // console.log('helloAnswers', allAnswersArray);
+   
+    //console.log('hello', allQuestions);
+
+    let allAnswersArray;
+    //   const answerArray = [allQuestions[0].answerTags];
+    // if (allAnswersArray = [...allQuestions.answerTags]) {
+    //     console.log('helloAnswers', allAnswersArray);
+    // }
+    // else {
+    //     console.log('array is null');
+    // }
+    // {allAnswersArray ? (console.log('helloAnswers', allAnswersArray)) : (console.log(err))}
+    // console.log('helloAnswers', answerArray);
+    const AnswersArray = allQuestions ? [allQuestions[0].answerTags] : '';
+    console.log('3rd question', AnswersArray);
+
+    
     return (    
         <div className="question-container">
                 <form onSubmit={handleSubmit} className="question" method="post">
@@ -90,7 +117,7 @@ function Question() {
                             <FormLabel sx={{ marginLeft: "0.7rem" }}>
                                 {allQuestions ? (
                                     <Box>
-                                        {allQuestions[1].question}
+                                        {allQuestions[0].question}
                                     </Box>
                                 
                                 
@@ -103,14 +130,52 @@ function Question() {
                                     <ButtonGroup
                                         orientation="vertical"
                                         aria-label="vertical contained button group"
-                                        variant="text" 
-                                    >
-                                        <Button variant="contained" sx={{ my: 1 }} onClick={handleData}>Answer</Button>
-                                        <Button variant="contained" sx={{ my: 1 }} onClick={handleData}>Answer 1</Button>
-                                        <Button variant="contained" sx={{ my: 1 }} onClick={handleData}>Answer 2</Button>
+                                        variant="text"
+                                        onClick={() => handleData()}
+                            >
+                                {
+                                    Array.isArray(AnswersArray) ?
+                                    AnswersArray.map((answer, index) => (
+                                    <Button variant="contained" sx={{ my: 1 }} key={index} name={index}>{answer}</Button>
+                                    ))
+                                        : console.log("Not an array")
+                                }
+                                
+                                {/* {
+                                    Array.isArray(AnswersArray) ?
+                                    AnswersArray.map((answer, index) => {
+                                    return <Button variant="contained" sx={{ my: 1 }} key={index}>{answer}</Button>;
+                                    })
+                                : console.log("Not an array")
+                                }   */}
+                                {/* {
+                                    props.AnswersArray.map((name, index) => {
+                                        return <Button key={index}>{name}</Button>;
+                                    })
 
+                                  
+                                    } */}
+                                    
+                                         {/* <Button variant="contained" sx={{ my: 1 }} >Answer 2</Button>
+                                         <Button variant="contained" sx={{ my: 1 }} >Answer 3</Button>
+                                         <Button
+                                             variant="contained"
+                                             fullWidth
+                                             type="submit"
+                                        >
+                                                 Answer 4
+                                        </Button>
+                                        
+                                            {allQuestions.question.map((answer, index) => (
+                                                <Button key={index} onClick={setNextQuestion}>{answer}</Button>
+                                            ))} */}
+                                         
                                     </ButtonGroup>
-                                    <div>
+                            <div>
+                                
+                                {/* // {AnswersArray.map((color, index) => (
+                                //     <button key={index}>{color}</button>
+                                // ))} */}
                                         {/* {QuestionAnswersArray ? (
                                         <div>
                                             {QuestionAnswersArray.map((item, index) => (
