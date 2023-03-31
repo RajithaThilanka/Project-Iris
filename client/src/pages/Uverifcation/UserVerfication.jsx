d4d5b96935a7c411086be7c92cf9e6955066ab8a
Rajitha <rajithat1998@gmail.com>
2023-03-31 22:51:50 +0530

    verification UI

---
+++ client/src/pages/Uverifcation/UserVerfication.jsx
@@ -0,0 +1,209 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import { Stack, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Navbar from "../../components/Appbar/Navbar";
+import { useEffect } from "react";
+import { getUser } from "../../components/api/UserRequests";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+const steps = [
+  "Required Documents",
+  "Identity Verification",
+  "Facial Recognition",
+];
+
+export default function UserVerfication() {
+  const [profile, setProfile] = useState(null);
+
+  const {
+    data: { user },
+  } = useSelector((state) => state.authReducer.authData);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const {
+          data: {
+            data: { data },
+          },
+        } = await getUser(id);
+        setProfile(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={10}
+      >
+        <Grid container item xs={12}>
+          <Navbar user={user} />
+        </Grid>
+        <Grid container item xs={12} sx={{ justifyContent: "center" }}>
+          <Box
+            sx={{
+              width: "1000px",
+              padding: "10px",
+              marginTop: "20px",
+            }}
+          >
+            <Stepper activeStep={activeStep}>
+              {steps.map((label, index) => {
+                const stepProps = {};
+                const labelProps = {};
+
+                return (
+                  <Step key={label} {...stepProps}>
+                    <StepLabel {...labelProps}>{label}</StepLabel>
+                  </Step>
+                );
+              })}
+            </Stepper>
+
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Stack
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Typography variant="h5" sx={{ mt: 2, mb: 1 }}>
+                    Under Review
+                  </Typography>
+
+                  <Typography>
+                    You will receive an email once review is completed
+                  </Typography>
+                </Stack>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {activeStep === 0 && (
+                  <Box
+                    sx={{
+                      height: "500px",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      padding: "10px",
+                    }}
+                  >
+                    <Stack
+                      direction="column"
+                      sx={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Typography variant="h5">
+                        Step 1: Required Documents
+                      </Typography>
+                      <Box>Required Documents Component</Box>
+                    </Stack>
+                  </Box>
+                )}
+                {activeStep === 1 && (
+                  <Box
+                    sx={{
+                      height: "500px",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Stack
+                      direction="column"
+                      sx={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Typography variant="h5">
+                        Step 2: Identity Verification
+                      </Typography>
+                      <Box>Identity VArification component</Box>
+                    </Stack>
+                  </Box>
+                )}
+                {activeStep === 2 && (
+                  <Box
+                    sx={{
+                      height: "500px",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Stack
+                      direction="column"
+                      sx={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Typography variant="h5">
+                        Step 3: Facial Recognition
+                      </Typography>
+                      <Box>Facial Recognition Component</Box>
+                    </Stack>
+                  </Box>
+                )}
+                <Box
+                  sx={{ display: "flex", flexDirection: "row", pt: 2 }}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={5}
+                  >
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={handleBack}
+                      variant="contained"
+                    >
+                      Back
+                    </Button>
+
+                    <Button onClick={handleNext} variant="contained">
+                      {activeStep === steps.length - 1 ? "Submit" : "Next"}
+                    </Button>
+                  </Stack>
+                </Box>
+              </React.Fragment>
+            )}
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
