f5851f0458b15be6436c85a2bc028edfd8cc7a14
Rajitha <rajithat1998@gmail.com>
2023-04-24 15:02:37 +0530

    update ciontroller

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -90,7 +90,7 @@ router.route('/names').get(authController.protect, userController.getUsers);
 router.route('/').get(
   authController.protect,
   //userSettingsController.checkSearchTokens,
-  authController.protect,
+
   userController.getUsers
 );
 router
