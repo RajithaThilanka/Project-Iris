a5dae3be69796032025a76ffeab04b122863badf
Rajitha <rajithat1998@gmail.com>
2023-05-25 07:46:23 +0530

    update

--- client/src/components/Profile/ProfileComplete/profilecomplete.jsx
+++ client/src/components/Profile/ProfileComplete/profilecomplete.jsx
@@ -3,9 +3,34 @@ import PropTypes from "prop-types";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { useContext, useEffect, useState } from "react";
+import { getMyVerStatus } from "../../../api/UserRequests";
 const ProfileData = require("../../Profile/profileData.json");
 
 function CircularProgressWithLabel(props) {
+  const [verStatus, setVerStatus] = useState(false);
+  useEffect(() => {
+    const getStatus = async () => {
+      try {
+        const {
+          data: {
+            data: { data },
+          },
+        } = await getMyVerStatus();
+
+        if (data) {
+          setVerStatus(data.status);
+        } else {
+          setVerStatus(false);
+        }
+        //   setVerStatus()
+      } catch (error) {
+        console.log(error);
+        setVerStatus(false);
+      }
+    };
+    getStatus();
+  }, []);
   return (
     <Box sx={{ position: "relative", display: "inline-flex" }}>
       <CircularProgress variant="determinate" {...props} />
